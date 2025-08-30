01e6b4ed44e2d13295ec3d4e5ae66f675eaf98af
haripriya91 <geetpriya.k@gmail.com>
2025-08-30 23:58:43 +0200

    Header Changes: CSS modifications & location dialogbox added

--- frontend/src/components/CountryDropdown/index.jsx
+++ frontend/src/components/CountryDropdown/index.jsx
@@ -1,5 +1,8 @@
-import Button from '@mui/material/Button';
+// Removed duplicate import
+import Dialog from '@mui/material/Dialog';
 import { FaAngleDown } from "react-icons/fa6";
+import { IoSearch } from "react-icons/io5";
+import Button from '@mui/material/Button';
 
 const CountryDropdown = () => {
     return (
@@ -11,6 +14,18 @@ const CountryDropdown = () => {
                 </div>
                 <span className='ml-auto'><FaAngleDown /> </span>
             </Button>
+
+
+            <Dialog open={true} className='locationDialog'> 
+                <h5>Choose your Delivery Location</h5>
+                <p>Enter your address and we will specify the offer for your area.</p>
+          
+                <div className="headerSearch w-100">
+                    <input type="text" placeholder='Search your area....' />
+                    <Button><IoSearch /></Button>
+                </div>
+          
+            </Dialog>
         </>
         
     );
